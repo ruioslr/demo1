17cb381c2d4ccb33cc57cb453ba2500ed06ecd48
lirui <lirui@createra.fun>
2023-08-23 15:34:03 +0800

    x

--- apps/main/lib/index.d.ts
+++ apps/main/lib/index.d.ts
@@ -1,3 +1,6 @@
 export declare const b = 2;
 export declare const a = 1;
 export declare const e: number;
+export declare const f = 5;
+export declare const g = 5;
+export declare const h = 5;
--- apps/main/lib/index.js
+++ apps/main/lib/index.js
@@ -2,3 +2,6 @@ import { c, d } from 'tools';
 export var b = 2;
 export var a = 1;
 export var e = c + d;
+export var f = 5;
+export var g = 5;
+export var h = 5;
